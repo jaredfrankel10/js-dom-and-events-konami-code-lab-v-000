0bacaa9e6d0f556c820b0d6f68754a341a000592
jared frankel <jaredfrankel10@gmail.com>
2018-02-02 14:37:58 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
-   
+
   document.body.addEventListener('keydown', function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
 
@@ -11,7 +11,7 @@ function init() {
       index++;
 
       if (index === code.length) {
-        alert('Konami Code!');  
+        alert('Konami Code!');
         index = 0;
       }
     } else {
